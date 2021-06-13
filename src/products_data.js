94cc98a3b9e07cf60ba5bfa99e298ef564bb116f
chai <wadhai.vijay.chaitanya@gmail.com>
2021-06-13 13:46:45 +0200

    formatting, finished

---
+++ src/products_data.js
@@ -0,0 +1,78 @@
+
+export default [
+  {
+    imageUrl:
+      "https://live.backmomente.de/wp-content/uploads/2018/05/weizenbroetchen-1024x576.jpg.webp",
+    name: "Weizenbrötchen",
+
+    price: 0.69,
+    bigOrderPrice: 0.39,
+    minimumOrder: 70,
+    canBigOrder: true,
+  },
+  {
+    imageUrl:
+      "http://3.bp.blogspot.com/--EVM3pX8Orw/T5qcGp4VVAI/AAAAAAAABB0/6lWx7lAK--s/s1600/CIMG1072.JPG",
+    name: "Weizenmischbrötchen",
+    price: 0.49,
+  },
+  {
+    imageUrl:
+      "https://img.chefkoch-cdn.de/rezepte/1102461216195342/bilder/734265/crop-960x720/roggenbroetchen.jpg",
+    name: "Roggenbrötchen",
+    price: 0.49,
+    bigOrderPrice: 0.25,
+    minimumOrder: 50,
+
+    canBigOrder: true,
+  },
+  {
+    imageUrl:
+      "https://www.petersgutebackstube.de/wp-content/uploads/Bioland-Vollkornbroetchen.jpg",
+    name: "Vollkornbrötchen",
+    price: 0.79,
+  },
+  {
+    imageUrl:
+      "https://www.brotpoet.de/wp-content/uploads/2018/10/M%C3%BChlen-Br%C3%B6tchen.jpg",
+    name: "Schrotbrötchen",
+    price: 0.49,
+  },
+  {
+    imageUrl:
+      "https://cdn.gutekueche.de/upload/rezept/6063/dinkelbroetchen.jpg",
+    name: "Dinkelbrötchen",
+    price: 0.69,
+    bigOrderPrice: 0.29,
+    minimumOrder: 50,
+
+    canBigOrder: true,
+  },
+  {
+    imageUrl:
+      "https://cdn.shopify.com/s/files/1/0080/3327/1871/products/Butter_Croissant_1024x1024.jpg?v=1570055727",
+    name: "Butter Croissant",
+    price: 0.49,
+  },
+  {
+    imageUrl: "https://data.rischart.de/images/40008.jpg",
+    name: "Schokocroissant",
+    price: 1.49,
+  },
+  {
+    imageUrl:
+      "http://buntes-potpourri.de/wp-content/uploads/2018/12/pizzabroetchen.jpg",
+    name: "Pizzabrötchen",
+    price: 2.19,
+  },
+  {
+    imageUrl:
+      "https://www.edeka-wucherpfennig.de/wp-content/uploads/2017/04/apfeltaschen.jpg",
+    name: "Apfeltasche",
+    price: 1.89,
+    bigOrderPrice: 1.25,
+    minimumOrder: 50,
+
+    canBigOrder: true,
+  },
+];
